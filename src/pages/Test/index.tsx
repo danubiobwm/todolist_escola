3a380a75ac1703ba0b3d19eb2c9d6c68ae102549
danubiobwm <danubio.bwm@gmail.com>
2021-08-18 21:24:21 -0300

    feat: :rocket: Feat/ConfigApp
    Feat/ConfigApp

---
+++ src/pages/Test/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Test: React.FC = () => {
+  return (
+    <div>
+      <h1>Escola TodoList</h1>
+    </div>
+  );
+};
+
+export default Test;
